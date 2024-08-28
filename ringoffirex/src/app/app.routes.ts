b261953121ffdcf4046b71808534f6e720845d9d
Jean Pondy <manbayesse@gmail.com>
2024-08-28 06:50:14 +0200

    Kartestapel mit ngFor anzeigen

--- ringoffirex/src/app/app.routes.ts
+++ ringoffirex/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { StartCreenComponent } from './start-creen/start-creen.component';
+import { GameComponent } from './game/game.component';
 
 export const routes: Routes = [
     { path: '', component: StartCreenComponent },
+    { path: 'game', component: GameComponent },
    
 ];
